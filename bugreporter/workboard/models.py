d5ada249593e3b3d1fe82943924d36c4d6ca0821
Arrun Patkunasingam <patkunaa@mcmaster.ca>
2020-09-12 19:51:04 -0400

    added model for Projects

--- bugreporter/workboard/models.py
+++ bugreporter/workboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- bugreporter/workboard/views.py
+++ bugreporter/workboard/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-projects = [
-    {
-        'creator': 'arrun1',
-        'project_name': 'Project 1',
-        'content': 'What is this project for?',
-        'date_created': 'Sept 10, 2020'
-    },
-    {
-        'creator': 'arrun2',
-        'project_name': 'Project 2',
-        'content': 'What is this second project for?',
-        'date_created': 'Sept 12, 2020'
-    }
-]
+from .models import Project
 
 
 def home(request):
     context = {
-        'projects': projects,
+        'projects': Project.objects.all(),
         'title': 'Projects'
     }
     return render(request, 'workboard/home.html', context)
