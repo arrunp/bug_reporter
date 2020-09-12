bf95741ca4291cfbf98a78755fde1395078e3162
Arrun Patkunasingam <patkunaa@mcmaster.ca>
2020-09-12 16:29:01 -0400

    adding html template

--- bugreporter/bugreporter/settings.py
+++ bugreporter/bugreporter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'workboard.apps.WorkboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- bugreporter/workboard/views.py
+++ bugreporter/workboard/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+projects = [
+    {
+        'creator': 'arrun1',
+        'project_name': 'Project 1',
+        'content': 'What is this project for?',
+        'date_created': 'Sept 10, 2020'
+    },
+    {
+        'creator': 'arrun2',
+        'project_name': 'Project 2',
+        'content': 'What is this second project for?',
+        'date_created': 'Sept 12, 2020'
+    }
+]
+
+
+def home(request):
+    context = {
+        'projects': projects
+    }
+    return render(request, 'workboard/home.html', context)
