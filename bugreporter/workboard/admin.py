de458d5a5d253ad9557d4f02280ef6e6f935d301
Arrun Patkunasingam <patkunaa@mcmaster.ca>
2020-09-13 14:01:02 -0400

    updates to migrations sqlite

--- bugreporter/workboard/admin.py
+++ bugreporter/workboard/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Bug, Task
 
 admin.site.register(Project)
+admin.site.register(Bug)
+admin.site.register(Task)
