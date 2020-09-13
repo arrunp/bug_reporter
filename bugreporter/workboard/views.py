15df0f9c9d65759ad706361be47c2e6a14e4fb81
Arrun Patkunasingam <patkunaa@mcmaster.ca>
2020-09-12 23:17:43 -0400

    updates to user profile

--- bugreporter/workboard/views.py
+++ bugreporter/workboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home(request):
     context = {
         'projects': Project.objects.all(),
